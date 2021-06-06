9b76bedbbc36f375376a2a5344c6c407001fcd9c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-06-06 14:04:44 -0300

    Requirement 1

--- server.js
+++ server.js
@@ -1,1 +1,55 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+// const randomAvatar = require('random-avatar');
+// const random = require('random-name')
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+// const clients = {};
+
+io.on('connection', (socket) => {
+  console.log(`novo usuário conectado! ${socket.id}`);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = new Date()
+      .toLocaleString({}, { hour12: true })
+      .replace(/\//g, '-');
+    const message = `${date} - ${nickname}: ${chatMessage}`;
+
+    io.emit('message', message);
+  });
+
+  //   socket.emit('confirmConnection', newUser);
+
+  //   socket.broadcast.emit('newUserConnect', newUser);
+
+  //   socket.on('sendMessageServer', (message) => {
+  //     const from = clients[socket.id];
+  //     io.emit('sendMessageToClients', { from, message });
+  //   });
+
+  //   socket.on('disconnect', () => {
+  //     const clientDisconnected = clients[socket.id];
+  //     delete clients[socket.id];
+  //     console.log(`cliente ${clientDisconnected.name} saiu do chat`);
+
+  //     io.emit('clientExit', clientDisconnected);
+  //   });
+});
+
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const PORT = 3000;
+
+http.listen(PORT, () => console.log('App listening on PORT %s', PORT));
